e484b388250cf97a14ea414a59cb8864c740a891
joshuadunaway23@gmail.com <joshuadunaway23@gmail.com>
2024-02-21 15:38:08 +0000

    wrote and used own simple macro and also used macro from dbt-utils

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -6,7 +6,8 @@ SELECT
     quantity, 
     CAST(purchase_price AS FLOAT64) AS purchase_price, 
     ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
-    ROUND(s.revenue - s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
+    ROUND(s.revenue - s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin,
+    {{ margin_percent('s.revenue', 's.quantity*CAST(p.purchase_price AS FLOAT64)')}} AS margin_percent
 FROM {{ref("stg_raw__sales")}} s
 LEFT JOIN {{ref("stg_raw__product")}} p 
 USING (products_id)
